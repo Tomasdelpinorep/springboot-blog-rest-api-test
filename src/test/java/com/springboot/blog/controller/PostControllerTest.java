10afee2783d6d15718708d24f72a29fd9b1cb27f
Fran Ruiz <fjavierrule@gmail.com>
2024-02-05 12:22:33 +0100

    empezar con el test del controlador

---
+++ src/test/java/com/springboot/blog/controller/PostControllerTest.java
@@ -0,0 +1,37 @@
+package com.springboot.blog.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createPost() {
+    }
+
+    @Test
+    void getAllPosts() {
+    }
+
+    @Test
+    void getPostById() {
+    }
+
+    @Test
+    void updatePost() {
+    }
+
+    @Test
+    void deletePost() {
+    }
+
+    @Test
+    void getPostsByCategory() {
+    }
+}
