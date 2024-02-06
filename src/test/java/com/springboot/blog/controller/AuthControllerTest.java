6eac15fb10b81e89052a640f60cde46965302caf
Alejandro <jimenezalejandro505@gmail.com>
2024-02-06 22:47:42 +0100

    test empezado

---
+++ src/test/java/com/springboot/blog/controller/AuthControllerTest.java
@@ -0,0 +1,32 @@
+package com.springboot.blog.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles({"test"})
+class AuthControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void register() {
+    }
+}
