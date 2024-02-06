bfc8be7d17b22dc7040c2e03217ea6edad596f2b
Fran Ruiz <fjavierrule@gmail.com>
2024-02-06 12:48:02 +0100

    repositorio arreglado

--- src/test/java/com/springboot/blog/controller/PostControllerTest.java
+++ src/test/java/com/springboot/blog/controller/PostControllerTest.java
@@ -20,6 +20,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -42,6 +43,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles({"test"})
 class PostControllerTest {
 
     @Autowired
