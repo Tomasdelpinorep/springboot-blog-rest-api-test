491c66133f9cfbb5db1995185720ce0fba991edc
Tomás del Pino Coffey <114216577+Tomasdelpinorep@users.noreply.github.com>
2024-02-07 12:56:55 +0100

    Delete target directory

--- target/test-classes/import-category.sql
+++
@@ -1,3 +0,0 @@
-delete from posts;
-delete from categories;
-insert into categories (id, name, description) values (1, 'Technology', 'Posts related to technology');
--- target/test-classes/import-posts.sql
+++
@@ -1,2 +0,0 @@
-delete from posts;
-INSERT INTO posts (id, title, description, content, category_id) VALUES (1000, 'Sample Title', 'Sample Description', 'Sample Content', 1);
